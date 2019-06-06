7b872929f8b969322fe8f948ca85576866f1675a
Dinesh Gowda <dinesh240894@gmail.com>
2019-06-06 14:23:42 +0530

    O(n)

--- PrimeNumbers.cpp
+++ PrimeNumbers.cpp
@@ -77,6 +77,34 @@ void segmentedseive(ll n)
 	}
 }
  
+
+/**
+Algorithm to generate prime numbers upto LMAX in O(n),
+The Sieve takes O(nlglgn).
+We init an array lp of size LMAX to 0.
+if lp[i]==0 we add it to pr list of primes.
+We multiply i with primes in pr and set lp[i*{pr[0..n]}=pr[0..n]
+Space Complexity O( n + n/lgn ) => Very Bad.
+**/
+
+#define LMAX 100000
+int lp[LMAX+1];
+vector<int> pr;
+
+void primes_with_linercomp(int N)
+{
+	for(int i=2;i<=LMAX;++i)
+	{
+		if(lp[i]==0)
+		{
+			lp[i]=i;
+			pr.pb(i);
+		}
+		
+		for(int j=0; j<(int)pr.size()&&i*pr[j]<=LMAX&&pr[j]<=lp[i];++j)
+			lp[i*pr[j]]=pr[j];
+	}
+}
 int main() {
 	segmentedseive(100000000);
 	return 0;
